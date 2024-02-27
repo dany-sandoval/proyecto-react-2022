7d994a771a24fdfb2329aef3db9f0a0108240088
Dany Sandoval <dasandovalal@gmail.com>
2024-02-27 13:09:00 -0700

    Se creo la lista de criptomonedas, se agrego Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+  
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        setCriptos(data.data.data);
+      })
+      .catch(() => {
+        console.log("La petición falló");
+      })
+  }, [])
+
+  if(!criptos) return <span>Cargando...</span>
 
   return (
     <>
-      <h1>¡HOLA MUNDO!</h1>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        { criptos.map(({id, name, priceUsd}) => (
+          <li key={id}>Nombre: {name} - Precio: {priceUsd}</li>
+        )) }
+      </ol>
     </>
   )
 }
